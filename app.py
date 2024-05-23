f88ff71cb5ce52f773ba33bda9075894a3efb741
Aymenmz <77240846+Aymenmz@users.noreply.github.com>
2024-05-23 16:55:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'Hello From Aymen, TY!!!'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
